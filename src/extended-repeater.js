e7ee627277a80b297324db70c63855a604fed45a
Anatoliy Komarov <admin@komaroff.biz>
2022-10-11 18:40:40 +0300

    feat: remive code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -18,26 +18,11 @@ const { NotImplementedError } = require('../extensions/index.js');
 function repeater(str, options) {
   console.log('str',str)
   console.log('options',options)
-  let string = String(str);
-  let arr = [];
+  let data = String(str)
+  let arr = []
 
-  if (options) {
-    let repeatTimes = options.repeatTimes || 1;
-    let separator = options.separator || '+';
-    let addition = String(options.addition) || '';
-    let additionRepeatTimes = options.additionRepeatTimes || 1;
-    let additionSeparator = options.additionSeparator || '|';
 
-    let substr = string + ((String(options.addition) !== 'undefined' ? addition + additionSeparator : '')).repeat(additionRepeatTimes);
-    if (String(options.addition) !== 'undefined') {
-      substr = substr.slice(0, substr.length - additionSeparator.length);
-    }
 
-    for (let i = 0; i < repeatTimes; i++) {
-      arr.push(substr);
-    }
-    return arr.join(separator);
-  }
 
 
   throw new NotImplementedError('Not implemented');
