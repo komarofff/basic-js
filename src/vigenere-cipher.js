8f334396b31703a2dd389a1bcb63997cdd730c80
Anatoliy Komarov <admin@komaroff.biz>
2022-10-17 11:00:25 +0300

    feat: finish vigener js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -54,17 +54,14 @@ class VigenereCipheringMachine {
         // }
     }
 
-    encrypt() {
+    encrypt(strArg, keyArg) {
 
-        if (!arguments[0] || !arguments[1]) {
-            throw new Error('No arguments')
+        if (!strArg || !keyArg)  {
+            throw new Error('Incorrect arguments!')
         } else {
             this.doMatrix()
-            let str = arguments[0].split('')
-            let key = arguments[1].split('')
-            //console.log('arguments', arguments)
-            //console.log('str', str)
-            //console.log('key', key)
+            let str = strArg.split('')
+            let key = keyArg.split('')
             let strArr = []
             for (let i = 0; i < str.length; i++) {
                 let symbolIndex = this.matrix[0].indexOf(str[i].toUpperCase())
@@ -74,17 +71,14 @@ class VigenereCipheringMachine {
                     strArr.push(str[i])
                 }
             }
-            //console.log('strArr', strArr.join(','))
             let keyArr = []
             for (let z = 0; z < key.length; z++) {
                 for (let q = 1; q < this.matrix.length; q++) {
                     if (key[z].toUpperCase() === this.matrix[q][0]) {
-                        //console.log(key[z],this.matrix[q][0],q)
                         keyArr.push(q)
                     }
                 }
             }
-            //console.log('keyArr', keyArr.join(','))
             let keyLength = key.length
             let counter = 0
             let result = []
@@ -99,24 +93,24 @@ class VigenereCipheringMachine {
                 }
 
             })
-            return result.join('')
+            if(this.type) {
+                return result.join('')
+            }else{
+                return  result.reverse().join('')
+            }
         }
 
         //throw new NotImplementedError('Not implemented');
         // remove line with error and write your code here
     }
 
-    decrypt() {
-        console.log('this.type',this.type)
-        if (!arguments[0] || !arguments[1]) {
-            throw new Error('No arguments')
+    decrypt(strArg,keyArg) {
+        if (!strArg || !keyArg)  {
+            throw new Error('Incorrect arguments!')
         } else {
             this.doMatrix()
-            let str = arguments[0].split('')
-            let key = arguments[1].split('')
-            console.log('arguments', arguments)
-            console.log('str', str)
-            console.log('key', key)
+            let str = strArg.split('')
+            let key = keyArg.split('')
 
             let keyArr = []
             for (let z = 0; z < key.length; z++) {
@@ -152,7 +146,11 @@ class VigenereCipheringMachine {
                 }
             })
             //LEARN FRONTEND DEVELOPMENT :)
-            return result.join('')
+            if(this.type) {
+                return result.join('')
+            }else{
+                return  result.reverse().join('')
+            }
         }
 
         //throw new NotImplementedError('Not implemented');
