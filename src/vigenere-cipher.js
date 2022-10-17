e9ed38ecd81f9b3295da7f141021e41526066101
Anatoliy Komarov <admin@komaroff.biz>
2022-10-17 09:56:12 +0300

    feat: add encrypt to vigener

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -45,25 +45,68 @@ class VigenereCipheringMachine {
             }
             let shift = this.symbolArray.shift()
             this.symbolArray.push(shift)
-            this.matrix[q+1] = newArr
+            this.matrix[q + 1] = newArr
         }
+
+        // for (let i = 0; i < this.matrix.length; i++) {
+        //     console.log(this.matrix[i].join(' '))
+        // }
     }
 
     encrypt() {
-        this.doMatrix()
-        if (!arguments[0] || !arguments[1]) throw new Error('No arguments');
-        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^%'
-        let arr = str.split('')
-        for (let i = 0; i < this.matrix.length; i++) {
-            console.log(this.matrix[i].join(' '))
 
+        if (!arguments[0] || !arguments[1]) {
+            throw new Error('No arguments')
+        } else {
+            this.doMatrix()
+            let str = arguments[0].split('')
+            let key = arguments[1].split('')
+            //console.log('arguments', arguments)
+            //console.log('str', str)
+            //console.log('key', key)
+            let strArr = []
+            for (let i = 0; i < str.length; i++) {
+                let symbolIndex = this.matrix[0].indexOf(str[i].toUpperCase())
+                if (symbolIndex > 0) {
+                    strArr.push(symbolIndex)
+                } else {
+                    strArr.push(str[i])
+                }
+            }
+            //console.log('strArr', strArr.join(','))
+            let keyArr = []
+            for (let z = 0; z < key.length; z++) {
+                for (let q = 1; q < this.matrix.length; q++) {
+                    if (key[z].toUpperCase() === this.matrix[q][0]) {
+                        //console.log(key[z],this.matrix[q][0],q)
+                        keyArr.push(q)
+                    }
+                }
+            }
+            //console.log('keyArr', keyArr.join(','))
+            let keyLength = key.length
+            let counter = 0
+            let result = []
+            strArr.forEach(el => {
+                let keyLength = key.length
+                if (typeof el === "number") {
+                    result.push(this.matrix[keyArr[counter]][el])
+                    counter++
+                    if (counter >= keyLength) counter = 0
+                } else {
+                    result.push(el)
+                }
+
+            })
+            return result.join('')
         }
+
         //throw new NotImplementedError('Not implemented');
         // remove line with error and write your code here
     }
 
     decrypt() {
-        console.log(arguments)
+        //console.log(arguments)
         throw new NotImplementedError('Not implemented');
         // remove line with error and write your code here
     }
